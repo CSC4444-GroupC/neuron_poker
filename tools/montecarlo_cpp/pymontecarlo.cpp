dc8fbd8f09f0b0771aafe047c814605ae0ab124a
Mikhalib Green <mikhalibgreen@gmail.com>
2025-12-02 08:33:43 -0600

    Fixed cppimport for linux

--- tools/montecarlo_cpp/pymontecarlo.cpp
+++ tools/montecarlo_cpp/pymontecarlo.cpp
@@ -1,5 +1,15 @@
+// cppimport
 <%
-cfg['compiler_args'] = ['/std:c++17']
+import sys
+
+# Detect platform
+if sys.platform.startswith("win"):
+    # MSVC uses /std:c++17
+    cfg['compiler_args'] = ['/std:c++17']
+else:
+    # GCC/Clang use -std=c++17
+    cfg['compiler_args'] = ['-std=c++17']
+
 setup_pybind11(cfg)
 cfg['sources'] = ['Montecarlo.cpp']
 %>
@@ -8,7 +18,7 @@ cfg['sources'] = ['Montecarlo.cpp']
 #include <pybind11/pybind11.h>
 
 #include <pybind11/stl.h>
-#include <pybind11/complex.h>,
+#include <pybind11/complex.h>
 #include <pybind11/functional.h>
 #include <pybind11/chrono.h>
 
